68b14dc880b334e5dfaa14ff7fa778b61b5fe451
unknown <elotgodsplan2@gmail.com>
2025-02-15 16:19:43 +0100

    Create MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,8 @@ import "./App.css";
 function App() {
   return (
     <>
-     <Text display="hello" />
-     <Text  display="whats up" />
-    </>
-  );
-}
 
-function Text({display}) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
+    </>
   );
 }
 
