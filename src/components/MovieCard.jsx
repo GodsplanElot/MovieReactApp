4dfc6f419646f0948fc387dfe39fb05000dfd22d
unknown <elotgodsplan2@gmail.com>
2025-02-15 16:33:27 +0100

    created a prop to handle a click event

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,16 @@
 function MovieCard({ movie }) {
+
+    function onFavoriteClick() {
+        
+    }
+
   return (
     <>
       <div className="movie-card">
         <div className="movie-poster">
-            
+            <img src={movie.url} alt="movie.title" />
+            <div className="movie-overly" />
+                <button className="favourite-btn" onClick={onFavoriteClick}></button>
         </div>
       </div>
     </>
