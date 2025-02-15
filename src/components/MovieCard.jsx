4e2c646f4b130adf41a3e48a2d3b102a3e6a7224
unknown <elotgodsplan2@gmail.com>
2025-02-15 16:37:35 +0100

    Fix typo in Syntax Error

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ function MovieCard({ movie }) {
       <div className="movie-card">
         <div className="movie-poster">
             <img src={movie.url} alt="movie.title" />
-            <div className="movie-overly" />
+            <div className="movie-overlay" />
                 <button className="favourite-btn" onClick={onFavoriteClick}>
                     ❤
                 </button>
