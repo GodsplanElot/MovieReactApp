d2f58a75dfac9e84ab49f4f0f0b46be707718245
unknown <elotgodsplan2@gmail.com>
2025-02-16 23:36:19 +0100

    Update app.jsx, used Short Cuirting for conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import "./App.css";
 import MovieCard from "./components/MovieCard";
 
 function App() {
+  const movieNumber = 1;
+
   return (
     <>
-      <MovieCard movie={{title: "Elot's Film", release_date: "2024"}} />
-      <MovieCard movie={{title: "kaka's Film", release_date: "2022"}} />
+      {movieNumber === 1 && <MovieCard movie={{title: "Elot's Film", release_date: "2024"}} /> }
+      
+      {/* <MovieCard movie={{title: "kaka's Film", release_date: "2022"}} /> */}
     </>
   );
 }
