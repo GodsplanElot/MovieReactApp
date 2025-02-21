21d66164706990361c54f5dfe13113aeb9a30b96
unknown <elotgodsplan2@gmail.com>
2025-02-21 23:47:30 +0100

    Add text to favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,6 +2,8 @@ function Favorite() {
     return (
         <>
             <div className="favorites-empty">
+                <h2>No Favorites Movies yet</h2>
+                <p>Start adding movies to your favorites and they will appaer here</p>
                 
             </div>
         </>
